736d26af98f3730567dd3d09ef5301da0bf989a4
Jhondistorsion <jonatan.ssp87@gmail.com>
2021-09-21 16:40:51 +0200

    Version 1.1

---
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.contador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contador;
+    private TextView numero = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contador = 0;
+        numero = (TextView) findViewById(R.id.txtnumero);
+    }
+
+    public void contar(View view) {
+        contador++;
+        numero.setText(String.valueOf(contador));
+
+    }
+
+    public void reset(View view) {
+        contador = 0;
+        numero.setText(String.valueOf(contador));
+    }
+}
